3f6130efb411b79f0a67cc20a4ae708c1d3e160b
Vlad Pavlichuk <exoit2014@vr-protect.email>
2023-04-15 12:53:34 +0300

    feat: подключение редакса к проекту/роутинг

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { Suspense, lazy } from 'react';
+import ErrorBoundary from '@/common/error-boundary';
+import { Route, Routes } from 'react-router-dom';
+import { AppRoutes } from '@/common/routes';
+import PacmanLoader from 'react-spinners/PacmanLoader';
+
+const MainPage = lazy(() => import('../common/pages/main-page'));
 
 const App = () => {
-    return <h1>Инициализация</h1>;
+  return (
+    <ErrorBoundary>
+      <Suspense fallback={<PacmanLoader />}>
+        <Routes>
+          <Route path={AppRoutes.Any} element={<MainPage />} />
+        </Routes>
+      </Suspense>
+    </ErrorBoundary>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { createAppStore } from './app/store';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './app/app';
 import './styles/index.scss';
 
-render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container!);
+const store = createAppStore();
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
